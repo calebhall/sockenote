2202c83c0852dec95b7b894ea76abd375917e225
caleb Hallgit config --global user.email chall.19@churchillschool.com <chall.19@air-chall19.fios-router.home>
2018-09-27 18:13:44 -0400

    remove instagram api module

--- index.js
+++ index.js
@@ -13,59 +13,56 @@ var express = require('express'),
 // var config = JSON.parse(
 //     fs.readFileSync(configFile));
 
-
-
 app.use(express.static(__dirname + "/public"));
 server.listen(port, function() {
     console.log("listening on port", port);
 });
 var Twit = require("twit"),
     T = new Twit({
-        consumer_key: process.env.CONSUMER_KEY,// || config.CONSUMER_KEY,
-        consumer_secret: process.env.CONSUMER_SECRET,// || config.CONSUMER_SECRET,
-        access_token: process.env.ACCESS_TOKEN,// || config.ACCESS_TOKEN,
-        access_token_secret: process.env.ACCESS_TOKEN_SECRET// || config.ACCESS_TOKEN_SECRET
+        consumer_key: process.env.CONSUMER_KEY, // || config.CONSUMER_KEY,
+        consumer_secret: process.env.CONSUMER_SECRET, // || config.CONSUMER_SECRET,
+        access_token: process.env.ACCESS_TOKEN, // || config.ACCESS_TOKEN,
+        access_token_secret: process.env.ACCESS_TOKEN_SECRET // || config.ACCESS_TOKEN_SECRET
     });
 
-var Client = require("instagram-private-api").V1;
-var device = new Client.Device("sockenote");
-var storage = new Client.CookieFileStorage(__dirname + "/cookies/someuser.json");
+// var Client = require("instagram-private-api").V1;
+// var device = new Client.Device("sockenote");
+// var storage = new Client.CookieFileStorage(__dirname + "/cookies/someuser.json");
 
-function login_to_igrm_and_upload_image(stream, caption) {
-    var username = process.env.INSTAGRAM_USERNAME;
-    var password = process.env.INSTAGRAM_PASSWORD;
-    Client.Session.create(device, storage, username, password)
-        .then(function(session) {
-            return [session, Client.Account.searchForUser(session, "instagram")]
-        })
-        .spread(function(session, account) {
-            upload_photo_to_ingrm(session, stream, caption)
-        })
-}
+// function login_to_igrm_and_upload_image(stream, caption) {
+//     var username = process.env.INSTAGRAM_USERNAME;
+//     var password = process.env.INSTAGRAM_PASSWORD;
+//     Client.Session.create(device, storage, username, password)
+//         .then(function(session) {
+//             return [session, Client.Account.searchForUser(session, "instagram")]
+//         })
+//         .spread(function(session, account) {
+//             upload_photo_to_ingrm(session, stream, caption)
+//         })
+// }
 
-function upload_photo_to_ingrm(session, stream, caption){
-    var path = __dirname +"/test.jpg";
-    fs.writeFile(path, stream, "base64", function(writeErr){
-        Client.Upload.photo(session, path)
-        .then(function(upload){
-            return Client.Media.configurePhoto(session, upload.params.uploadId, caption);
-        })
-        .then(function(medium){
-            fs.unlinkSync(path);
-        })
-    })
-}
+// function upload_photo_to_ingrm(session, stream, caption){
+//     var path = __dirname +"/test.jpg";
+//     fs.writeFile(path, stream, "base64", function(writeErr){
+//         Client.Upload.photo(session, path)
+//         .then(function(upload){
+//             return Client.Media.configurePhoto(session, upload.params.uploadId, caption);
+//         })
+//         .then(function(medium){
+//             fs.unlinkSync(path);
+//         })
+//     })
+// }
+
 wss.on("connection", function(ws) {
 
     ws.on("message", function incoming(message) {
 
         var data = JSON.parse(message);
-
         if (!data.hasOwnProperty("event")) {
             console.log("no event");
             ws.send("no event");
             return;
-
         }
 
         if (data.event == "share") {
@@ -80,7 +77,7 @@ wss.on("connection", function(ws) {
                     }
                     T.post("statuses/update", params, function(err, res, response) {
 
-                        login_to_igrm_and_upload_image(data.image, data.tweet);
+                        // login_to_igrm_and_upload_image(data.image, data.tweet);
                     })
                 })
             } else
